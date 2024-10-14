5a9fefa34d8ca0924616a971b3224a2919d2538d
Ofghanirre <ofghanirre@gmail.com>
2024-10-14 02:12:14 +0200

    update main.c tests

--- main.c
+++ main.c
@@ -22,9 +22,19 @@ int sortByA(const void* a, const void* b) {
 
 void freePacket(void* packet) {}
 
-void packetTest() {
+
+int main(void) {
+    /**
+     * FIRST TEST
+     */
     CList list;
     clist_init(&list);
+
+    clist_println(&list, printPacket);
+
+    /**
+     * SECOND TEST
+     */
     TestPacket packet[]= {
         { .a = 1, .b = 2 },
         { .a = 4, .b = 5 },
@@ -34,32 +44,56 @@ void packetTest() {
     for (int i = 0; i < 4; i++) {
         clist_append(&list, &packet[i]);
     }
-    clist_print(&list, printPacket);
+    clist_println(&list, printPacket);
 
+    /**
+     * THIRD TEST
+     */
+    TestPacket packetToBeRemoved = {.a = 42, .b = 42 };
+    clist_append(&list, &packetToBeRemoved);
+    clist_println(&list, printPacket);
+    TestPacket * removedPacket;
+    clist_removeLast(&list, (void**) &removedPacket);
+    printf("Expected packet to be removed : %p\nRemoved packet : %p\n", &packetToBeRemoved, removedPacket);
+
+    /**
+     * FOURTH TEST
+     */
     CList filtered;
     clist_filter(&list, &filterEven, &filtered);
     clist_print(&filtered, printPacket);
 
+    /**
+     * FIFTH TEST
+     */
     CList sorted;
     clist_sort(&list, &sortByA, &sorted);
     clist_print(&sorted, printPacket);
 
+    /**
+     * SIXTH TEST
+     */
     CList reverse;
     clist_reverse(&sorted, &reverse);
     clist_print(&reverse, printPacket);
 
+    /**
+     * SEVENTH TEST
+     */
     CList merged;
     clist_merge(&sorted, &reverse, &merged);
     clist_print(&merged, printPacket);
 
-    clist_free(&list, &freePacket);
-    clist_free(&filtered, &freePacket);
-    clist_free(&sorted, &freePacket);
-    clist_free(&reverse, &freePacket);
-    clist_free(&merged, &freePacket);
-}
 
-int main(void) {
-    packetTest();
+    clist_free(&list, &freePacket);
+    // TEST 4
+    clist_free(&filtered, &freePacket);
+    // TEST 5
+    clist_free(&sorted, &freePacket);
+    // TEST 6
+    clist_free(&reverse, &freePacket);
+    // TEST 7
+    clist_free(&merged, &freePacket);
+
     return 0;
 }
